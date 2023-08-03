1d6d243cb47d131b0802da2f97434a730a29ff7c
Artem Bolotov <teeemon@gmail.com>
2023-08-03 13:39:16 +0300

    Initial Commit

---
+++ Roll Dice/Roll_DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roll_DiceApp.swift
+//  Roll Dice
+//
+//  Created by artembolotov on 03.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Roll_DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
